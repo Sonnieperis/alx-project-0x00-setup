4857920470aaf737843470f528dcad8962c7279b
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-11-16 12:11:02 +0300

    adding functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,15 +1,16 @@
+import { PillProps } from "@/interfaces";
 import React from "react";
 
 type PillProps = {
   title?: string;
 };
 
-const Pill: React.FC<PillProps> = ({ title = "Title" }) => {
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className="text-sm">{title}</p>
     </div>
-  );
-};
+  )
+}
 
 export default Pill;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,11 @@
+import Card from "@/components/Card"
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
-    </div>
-  );
-};
 
-export default Landing;
+      <Card />
+    </div>
+  )
+}
+export default Landing
